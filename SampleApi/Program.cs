01f6e0efac4e0ee2138fcb58b341be862852bdce
MRA <mohammadreza.abp@gmail.com>
2025-06-08 15:04:35 +0330

    change

--- SampleApi/Program.cs
+++ SampleApi/Program.cs
@@ -13,7 +13,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+//comment
 var summaries = new[]
 {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
